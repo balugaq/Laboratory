cb3f080f63382df2d3282a801bec3d46c26ffdf3
balugaq <129668183+balugaq@users.noreply.github.com>
2025-01-08 14:41:11 +0800

    Resolves #24
    可能修复了

--- src/main/java/me/Freeze_Dolphin/lab/Laboratory.java
+++ src/main/java/me/Freeze_Dolphin/lab/Laboratory.java
@@ -18,6 +18,7 @@ import me.Freeze_Dolphin.lab.listeners.ElectricAxe;
 import me.Freeze_Dolphin.lab.listeners.ElectricDigger;
 import me.Freeze_Dolphin.lab.listeners.EmeraldCapacity;
 import me.Freeze_Dolphin.lab.listeners.JukeboxDisc;
+import me.Freeze_Dolphin.lab.listeners.MimungBlaster;
 import me.Freeze_Dolphin.lab.listeners.NoDyeableSheep;
 import me.Freeze_Dolphin.lab.listeners.PlasmaCell;
 import me.Freeze_Dolphin.lab.listeners.RadioactivityWeapons;
@@ -157,6 +158,7 @@ public class Laboratory extends JavaPlugin implements SlimefunAddon {
         getServer().getPluginManager().registerEvents(new JukeboxDisc(), this);
         getServer().getPluginManager().registerEvents(new RadioactivityWeapons(), this);
         getServer().getPluginManager().registerEvents(new WitherProofArmors(), this);
+        getServer().getPluginManager().registerEvents(new MimungBlaster(), this);
 
         if (Variables.cfg.getBoolean("bugfixers.speed-ingot")) {
             getServer().getPluginManager().registerEvents(new SpeedIngotBUG(), this);
--- src/main/java/me/Freeze_Dolphin/lab/listeners/MimungBlaster.java
+++ src/main/java/me/Freeze_Dolphin/lab/listeners/MimungBlaster.java
@@ -42,9 +42,9 @@ public class MimungBlaster implements Listener {
             String sl = l.get(statusLine);
             if (sl.matches(U.color("(.*)&a(.*)"))) {
                 if (sl.split(U.color("&a"))[1].equals("正常")) {
-                    return -1;
+                    return -1; // "核心状态: &a正常"
                 } else return -5;
-            } else if (sl.matches(U.color("(.*)&b冷却中(.*)"))) {
+            } else if (sl.matches(U.color("(.*)&b冷却中(.*)"))) { // "核心状态: &b冷却中 (50 %)"
                 return Integer.parseInt(sl.split(U.color("&b冷却中 &7("))[1].split(" %)")[0]);
             } else return -5;
         } else return -10;
