c0113342514ae3e33e8562647e2142ac9c62ad02
balugaq <129668183+balugaq@users.noreply.github.com>
2025-01-15 12:18:19 +0800

    Resolves #53

--- src/main/java/me/Freeze_Dolphin/lab/Laboratory.java
+++ src/main/java/me/Freeze_Dolphin/lab/Laboratory.java
@@ -279,7 +279,7 @@ public class Laboratory extends JavaPlugin implements SlimefunAddon {
 
                     if (item instanceof Rechargeable rechargeable) {
                         rechargeable.removeItemCharge(itemStack, 0.01f);
-                        p.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 2, 2, true, false, true));
+                        p.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 100, 2, true, false, true));
                         p.updateInventory();
                     }
                 }
