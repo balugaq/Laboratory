08256f08cbab0ae6523dc013751e52f33f657b87
balugaq <129668183+balugaq@users.noreply.github.com>
2025-01-15 19:37:01 +0800

    Resolves #58

--- src/main/java/me/Freeze_Dolphin/lab/Tech.java
+++ src/main/java/me/Freeze_Dolphin/lab/Tech.java
@@ -1050,7 +1050,7 @@ public class Tech {
                         "&4终级发电机",
                         "&8⇨ &e⚡ &7768 J/s"));
 
-        new AdvancedAContainer(e, EGG_GENERATOR, NON, new ItemStack[] {
+        new AdvancedAContainer(e, EGG_GENERATOR, RecipeType.ENHANCED_CRAFTING_TABLE, new ItemStack[] {
                 SUNNARIUM_ALLOY, SUNNARIUM_ALLOY, SUNNARIUM_ALLOY,
                 new ItemStack(Material.END_STONE), new ItemStack(Material.FURNACE), new ItemStack(Material.END_STONE),
                 SUNNARIUM_ALLOY, SUNNARIUM_ALLOY, SUNNARIUM_ALLOY
